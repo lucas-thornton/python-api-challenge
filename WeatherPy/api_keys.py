310b57227711a0aa6fefce883d4a7c4f16c1072f
lucas-thornton <le.thornton644@gmail.com>
2020-07-16 01:30:17 +0800

    update

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "a29605c9ab299fda1db28d3f96415c23"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCeCgFBnfA61J9yAo-OGwp0ElT6tAEH06g"
